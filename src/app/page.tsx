0e2c08dc81f6c4228e2b46c42781193e2cd75234
PaulBuske <paul.buske@yahoo.com>
2025-04-15 17:35:40 -0400

    add fun videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,41 @@ import UserTable from "@/app/components/UserTable";
 export default function Home() {
   return (
       <Box>
+          <Box position="relative" width="100%" height="50vh" overflow="hidden" >
+              {/* YouTube Video Background */}
+              <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/zqLEO5tIuYs?autoplay=1&mute=1&loop=1&playlist=zqLEO5tIuYs"
+                  title="YouTube video background"
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media"
+                  style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: "100%",
+                      zIndex: -1,
+                      pointerEvents: "none",
+                  }}
+              ></iframe>
+
+              {/* Title Text */}
+              <Box
+                  position="absolute"
+                  top="40%"
+                  left="25%"
+                  textAlign="center"
+                  color="white"
+              >
+                  <Typography variant="h1" component="h1" align='center'>
+                      CSCI 4750 Group 5 Project Title: TBD
+                  </Typography>
+                  <Typography variant="h2">Spring 2025</Typography>
+              </Box>
+          </Box>
           <Stack spacing={2}>
-              <Typography variant="h1" component="h1">
-                  CSCI 4750 Group 5 Project Title: TBD
-              </Typography>
               <Typography variant="h2">
                   Spring 2025
               </Typography>
@@ -39,6 +70,18 @@ export default function Home() {
               <Typography variant='h3'>Current Users:</Typography>
               <UserTable/>
           </Stack>
+          <Box marginTop={4} display="flex" justifyContent="center">
+              <iframe
+                  width="1920"
+                  height="1080"
+                  src="https://www.youtube.com/embed/0aBkyfz9anQ?autoplay=1&mute=1"
+                  title="He-Man!"
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media"
+                  allowFullScreen
+              ></iframe>
+          </Box>
+          https://www.youtube.com/watch?v=zqLEO5tIuYs&ab_channel=brmbrmcar
       </Box>
     );
 }
