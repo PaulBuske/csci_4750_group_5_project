9c87c0decfb1082dadc371e2b3208d204a7612f6
paul.buske <paul.buske@yahoo.com>
2025-04-21 12:10:54 -0400

    create theme-registry, theme, fix home page and home page layout, fix login route, add example env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import {Box, Button, Typography} from '@mui/material';
 import React from "react";
-import React from "react";
-import NextLink from 'next/link';
 
 export default function Home() {
 
